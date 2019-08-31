7ddc96848d4f2990efd5e4cfa9b6e9b10dc0ce05
sndnvaps <sndnvaps@gmail.com>
2019-09-01 01:05:41 +0800

    make it can build with vs2017 msbuild

---
+++ admin/appveyor/appveyor.bat
@@ -0,0 +1,13 @@
+rem automatically go to particular toolset based in where compiler is by defaults.
+set MSBUILD_LOGGER=/logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+goto %TOOLSET%
+
+:msbuild
+cd build
+msbuild /m:2 /v:n /p:Platform=%ARCH% /p:Configuration="%CONFIGURATION%" SQLite3Secure_vc15.sln %MSBUILD_LOGGER%
+goto :eof
+
+:error
+echo.
+echo --- Build failed !
+echo.
